8b1a814d96952b8aff3d4b70e0ecaaec448f6f6e
gqzang <gqzang@gmail.com>
2021-11-03 12:29:27 +0000

    Use the exported file from Android phone 'Misc Links.txt' as update file.

--- novel/update.py
+++ novel/update.py
@@ -1,23 +1,25 @@
 #! /usr/bin/python3
 import os, re, contextlib as ctx
 
-with ctx.suppress(Exception), open('./update.txt', 'r') as fp:
-    lns = [re.sub('[\n ]', '', x) for x in fp.readlines() if len(x) > 1]
-tt = {lns[2*i]: lns[2*i+1] for i in range(int(len(lns)/2))}
+src = './Misc Links.txt'
+tar_dir = './Dolphin/'
 
-bfiles = [f for f in os.listdir('./Dolphin') if f.startswith('Bookmarks')]
-bfiles.sort()
-with ctx.suppress(Exception), open('./Dolphin/' + bfiles[-1], 'r') as fp:
+with ctx.suppress(Exception), open(src, 'r') as fp:
     lns = fp.readlines()
+a, b = [i for i, x in enumerate(lns) if '~'*10 in x]
+lns = [x for x in [re.sub('[\n\t ]', '', x) for x in lns[a+1:b]] if x]
+tt = {lns[i]: lns[i+1] for i in range(0, len(lns), 2)}
 
-ulns = []
-for line in lns:
-    if line.startswith('<DT><A HREF="') and line.endswith('</A>\n'):
-        a, b = line.rfind('">') + 2, line.rfind('=')
-        key = line[a:b].strip(' ')
-        if key in tt:
-            line = line[0:a] + key + ' = ' + tt[key] + '</A>\n'
-    ulns.append(line)
+bfs = sorted([f for f in os.listdir(tar_dir) if f.startswith('Book')])
+with ctx.suppress(Exception), open(tar_dir + bfs[-1], 'r') as fp:
+    ulns = []
+    for line in fp.readlines():
+        if line.startswith('<DT><A HREF="') and line.endswith('</A>\n'):
+            a, b = line.rfind('">') + 2, line.rfind('=')
+            key = line[a:b].strip(' ')
+            if key in tt:
+                line = line[0:a] + key + ' = ' + tt[key] + '</A>\n'
+        ulns.append(line)
 
-with ctx.suppress(Exception), open('./Dolphin/updated.html', 'w') as fp:
+with ctx.suppress(Exception), open(tar_dir + 'updated.html', 'w') as fp:
     fp.writelines(ulns)
