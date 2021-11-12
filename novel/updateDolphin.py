cc7a02c149958281a5ec2a282ae587242d445f3d
gz <gz@0.0.0.0>
2021-11-12 01:15:53 -0500

    Only update real new record, and point out old record.

--- novel/updateDolphin.py
+++ novel/updateDolphin.py
@@ -1,5 +1,19 @@
 import os, re, contextlib as ctx, requests
 
+
+def digit_v(rec):
+    if '-' not in rec:
+        return int(rec)
+    chp, sec = rec.split('-')
+    return int(chp) * 3000 + int(sec)
+
+
+def comp_rec(v1, v2):
+    if v1.lower() == 'end':
+        return 1
+    return digit_v(v1) - digit_v(v2)
+
+
 ciphers = 'TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:' \
           'TLS13-AES-256-GCM-SHA384:ECDHE:!COMPLEMENTOFDEFAULT'
 url = 'https://docs.google.com/document/d/' \
@@ -14,14 +28,20 @@ with ctx.suppress(Exception):
     tt = {lns[i]: lns[i+1] for i in range(0, len(lns), 2)}
 
 bfs = sorted([f for f in os.listdir(tar_dir) if f.startswith('Book')])
+nu = 0
 with ctx.suppress(Exception), open(tar_dir + bfs[-1], 'r') as fp:
     ulns = []
     for line in fp.readlines():
         if line.startswith('<DT><A HREF="') and line.endswith('</A>\n'):
             a, b, c = line.rfind('">') + 2, line.rfind('='), line.rfind('<')
-            key, val = line[a:b].strip(' '), line[b:c].strip(' ')
+            key, val = line[a:b].strip(' '), line[b+1:c].strip(' ')
             if key in tt:
-                line = line[0:a] + key + ' = ' + tt[key] + '</A>\n'
+                r = comp_rec(tt[key], val)
+                if r >= 0:
+                    line = line[0:a] + key + ' = ' + tt[key] + '</A>\n'
+                    nu += 1 if r > 0 else 0
+                else:
+                    print(key + ' = ' + tt[key] + ' < ' + val + '    old')
                 del tt[key]
         ulns.append(line)
 
@@ -31,4 +51,5 @@ with ctx.suppress(Exception), open(tar_dir + 'updated.html', 'w') as fp:
 if tt:
     for k, v in tt.items():
         print(k + ' = ' + v + '       Missing')
-pass
+
+print('\n' + str(nu) + ' records are updated.')
