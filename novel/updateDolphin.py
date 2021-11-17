111c60485c25645ad36e42be86e07503792dec74
gqzang <gqzang@gmail.com>
2021-11-17 15:39:34 +0000

    Code refactor.

--- novel/updateDolphin.py
+++ novel/updateDolphin.py
@@ -4,7 +4,7 @@ ciphers = 'TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:' \
           'TLS13-AES-256-GCM-SHA384:ECDHE:!COMPLEMENTOFDEFAULT'
 url = 'https://docs.google.com/document/d/' \
       '1aSKOYuXU1s08DJU0FahGBNXbrAFlgJ_YJ9v3EwN50kU/export?format=txt'
-tar_dir = os.path.dirname(os.path.abspath(__file__)) + '/Dolphin/'
+bdir = os.path.dirname(os.path.abspath(__file__)) + '/Dolphin/'
 xtr = lambda x: eval(x.lower().replace('end', '9'*6).replace('-', '*3000+'))
 
 with ctx.suppress(Exception):
@@ -12,26 +12,23 @@ with ctx.suppress(Exception):
     lns = requests.get(url).content.decode('utf-8').split('\n')
     a, b = [i for i, x in enumerate(lns) if '~'*10 in x][0:2]
     lns = [x for x in [re.sub('[\r\t ]', '', x) for x in lns[a+1:b]] if x]
-
-ulns, nu, tt = [], 0, {lns[i]: lns[i+1] for i in range(0, len(lns), 2)}
-bfs = sorted([f for f in os.listdir(tar_dir) if f.startswith('Book')])
-with ctx.suppress(Exception), open(tar_dir + bfs[-1], 'r') as fp:
-    for line in fp.readlines():
-        if line.startswith('<DT><A HREF="') and line.endswith('</A>\n'):
-            a, b, c = line.rfind('">'), line.rfind('='), line.rfind('</')
-            key, val = line[a+2:b].strip(' '), line[b+1:c].strip(' ')
-            if key in tt:
-                u, v = xtr(tt[key]), xtr(val)
-                if u >= v:
-                    line = line[0:a+2] + key + ' = ' + tt[key] + '</A>\n'
-                    nu += 1 if u > v else 0
-                else:
-                    print(key + ' = ' + tt[key] + ' < ' + val + '    old')
-                del tt[key]
-        ulns.append(line)
-for k, v in tt.items():
-    print(k + ' = ' + v + '       Missing')
-print('\n' + str(nu) + ' records are updated.')
-
-with ctx.suppress(Exception), open(tar_dir + 'updated.html', 'w') as fp:
-    fp.writelines(ulns)
+    ulns, nu, tt = [], 0, {lns[i]: lns[i+1] for i in range(0, len(lns), 2)}
+    with open(bdir + max([f for f in os.listdir(bdir)]), 'r') as fp:
+        for line in fp.readlines():
+            if line.startswith('<DT><A HREF="') and line.endswith('</A>\n'):
+                a, b, c = line.rfind('">'), line.rfind('='), line.rfind('</')
+                key, val = line[a+2:b].strip(' '), line[b+1:c].strip(' ')
+                if key in tt:
+                    u, v = xtr(tt[key]), xtr(val)
+                    if u >= v:
+                        line = line[0:a+2] + key + ' = ' + tt[key] + '</A>\n'
+                        nu += 1 if u > v else 0
+                    else:
+                        print(key + ' = ' + tt[key] + ' < ' + val + '   old')
+                    del tt[key]
+            ulns.append(line)
+    with open(bdir + 'Adjusted.html', 'w') as fp:
+        fp.writelines(ulns)
+    for k, v in tt.items():
+        print(k + ' = ' + v + '       Missing')
+    print('\n' + str(nu) + ' records are updated.')
