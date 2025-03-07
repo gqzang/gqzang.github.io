7c6c3a83cc3db99518ddfa4ae8f1a6f8ff5508c9
gqzang <gqzang@gmail.com>
2025-03-07 16:13:44 +0000

    A multiple line plotting example.

--- mj/mahjong.js
+++ mj/mahjong.js
@@ -235,32 +235,29 @@ function plot() {
   const canvas = document.getElementById('canvas')
   canvas.style.height = "70%"
 
-  const xyValues = [
-    {x:50, y:7},
-    {x:60, y:8},
-    {x:70, y:8},
-    {x:80, y:9},
-    {x:90, y:9},
-    {x:100, y:9},
-    {x:110, y:10},
-    {x:120, y:11},
-    {x:130, y:14},
-    {x:140, y:14},
-    {x:150, y:15}
-  ]
-  
+  const xValues = [100,200,300,400,500,600,700,800,900,1000];
+
   new Chart("chart", {
-    type: "scatter",
+    type: "line",
     data: {
+      labels: xValues,
       datasets: [{
-        pointRadius: 4,
-        pointBackgroundColor: "rgba(0,0,255,1)",
-        data: xyValues
+        data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
+        borderColor: "red",
+        fill: false
+      },{
+        data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
+        borderColor: "green",
+        fill: false
+      },{
+        data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
+        borderColor: "blue",
+        fill: false
       }]
     },
-    options:{    
+    options: {
       legend: {display: false},
       maintainAspectRatio: false
     }
-  })
+  });
 }
