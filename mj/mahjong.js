a18ca0d3646fff998e7aa8a350401c4f07a51396
gqzang <gqzang@gmail.com>
2025-03-07 15:50:20 +0000

    Make a example to use chart.js

--- mj/mahjong.js
+++ mj/mahjong.js
@@ -228,5 +228,39 @@ async function playMJ() {
 }
 
 function plot() {
-  alert("plot")
+  const gameLog = document.getElementById('game')
+  gameLog.rows = 6
+  gameLog.scrollTop = gameLog.scrollHeight
+
+  const chart = document.getElementById('chart')
+  // chart.canvas.parentNode.style.height = '200px'
+  
+  const xyValues = [
+    {x:50, y:7},
+    {x:60, y:8},
+    {x:70, y:8},
+    {x:80, y:9},
+    {x:90, y:9},
+    {x:100, y:9},
+    {x:110, y:10},
+    {x:120, y:11},
+    {x:130, y:14},
+    {x:140, y:14},
+    {x:150, y:15}
+  ]
+  
+  new Chart("chart", {
+    type: "scatter",
+    data: {
+      datasets: [{
+        pointRadius: 4,
+        pointBackgroundColor: "rgba(0,0,255,1)",
+        data: xyValues
+      }]
+    },
+    options:{    
+      legend: {display: false},
+      maintainAspectRatio: false
+    }
+  })
 }
