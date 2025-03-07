c0d0cf72a84337fc6d70b531f16330fe923fd184
gqzang <gqzang@gmail.com>
2025-03-07 16:36:23 +0000

    Implement of histry chart for each game.

--- mj/mahjong.js
+++ mj/mahjong.js
@@ -235,25 +235,44 @@ function plot() {
   const canvas = document.getElementById('canvas')
   canvas.style.height = "70%"
 
-  const xValues = [100,200,300,400,500,600,700,800,900,1000];
+  var x = [], ys = [[], [], [], [], []] 
+  hist.forEach((e, i) => {
+    x.push(i)
+    for(let j = 0; j < 5; j ++)
+      ys[j].push(e[j])
+  })
 
   new Chart("chart", {
     type: "line",
     data: {
-      labels: xValues,
-      datasets: [{
-        data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
-        borderColor: "red",
-        fill: false
-      },{
-        data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-        borderColor: "green",
-        fill: false
-      },{
-        data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-        borderColor: "blue",
-        fill: false
-      }]
+      labels: x,
+      datasets: [
+        {
+          data: ys[0],
+          borderColor: "black",
+          fill: false
+        },
+        {
+          data: ys[1],
+          borderColor: "yellow",
+          fill: false
+        },
+        {
+          data: ys[2],
+          borderColor: "red",
+          fill: false
+        },
+        {
+          data: ys[3],
+          borderColor: "green",
+          fill: false
+        },
+        {
+          data: ys[4],
+          borderColor: "blue",
+          fill: false
+        }
+      ]
     },
     options: {
       legend: {display: false},
