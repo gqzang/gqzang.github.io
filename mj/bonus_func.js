41346a7a9d928780ff9d97ea1d512d0758cbc0e9
gz <gqzang@gmail.com>
2025-03-10 09:10:11 -0400

    Example of showing zipped image in GDrive on webpage.

---
+++ mj/bonus_func.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// async function get_url()
+
+function set_image_url() {
+  var id = bonus["0000"]
+
+  gapi.client.drive.files.get({
+    fileId: id,
+    alt: "media"
+  })
+  .then(res => res.body)
+  .then(blob => new JSZip().loadAsync(blob))
+  .then(zip => zip.file('bonus.jpg').async("blob"))
+  .then(blob => {
+    const imageURL = URL.createObjectURL(blob);
+    document.getElementById("image").src = imageURL
+    console.log(imageURL)
+  })
+}
+
+function load_bonus() {
+  gapi.load('client', () => {
+    gapi.client.init({
+      'apiKey': 'AIzaSyC9yLlWoUMsbxKM2EpVA48p6Vuv3Q8b0XE',
+      'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
+    })
+    .then(() => set_image_url())    
+  })
+}
