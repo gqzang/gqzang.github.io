fc0b3ca2a9ed896cc03beeb44ba36671c5cead2e
gz <gqzang@gmail.com>
2025-03-12 22:32:30 -0400

    Add notification sound when bonus becomes available.

--- mj/bonus_func.js
+++ mj/bonus_func.js
@@ -17,7 +17,14 @@ setInterval(() => {
   document.getElementById("timer").value = count
   document.getElementById("timer").innerText = count
   document.getElementById("timer").style.color = count > 0 ? "red" : "green"
-  if(count == 0 && end > 0 && !settingImageUrl) setProp("bonus", false, "lightgoldenrodyellow")
+  if(count == 0 && end > 0 && !settingImageUrl) {
+    if(document.getElementById("bonus").disabled == true) {
+      // for sound file, see: https://mixkit.co/free-sound-effects/notification/
+      const notificationSound = new Audio("./mixkit-magic-notification-ring-2344.wav")
+      notificationSound.play();  
+    }
+    setProp("bonus", false, "lightgoldenrodyellow")
+  }
 }, 1000)
 
 var bonusUrl = "", bonusKey = "", bonusG = {}
