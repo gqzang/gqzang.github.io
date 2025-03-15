5445094591e9e3cdbd163cdd7ee44768f1e365bd
gz <gqzang@gmail.com>
2025-03-15 13:15:03 -0400

    test "text-size-adjust: auto;" option for mobile devices.

--- mj/slides/slides.js
+++ mj/slides/slides.js
@@ -167,7 +167,7 @@ function showOff2(name, key, url) {
   const key_ = key.slice(0, -4)
   const title = '<html style="overscroll-behavior: none;"><head><title>' + key_ + '</title>' + script + '</head>'
   const style = 'background-size: contain; background-position: center; background-repeat: no-repeat; overscroll-behavior: none;'
-  const body = `<div onclick="window.opener.nextSlide_()">${key_}${("<br>" + "&nbsp;".repeat(20)).repeat(4)}</div>`
+  const body = `<div style="text-size-adjust: auto;" onclick="window.opener.nextSlide_()">${key_}${("<br>" + "&nbsp;".repeat(20)).repeat(4)}</div>`
   win.document.write(`${title}<body style="background-image: url(${url}); color: gold; ${style}">${body}</body></html>`)
   win.document.close() 
 }
