dc0b779aedccda4736ba6747e23adf0b980434be
gz <gqzang@gmail.com>
2025-03-16 15:31:57 -0400

    Load a video from zip_pv with encrypted id.

--- mj/video/video.js
+++ mj/video/video.js
@@ -6,9 +6,26 @@ const API_KEY = 'AIzaSyAoZfGbF6tOm2jQfdLNIEhZHp80n9EZ8GY'         // zip_p from 
 const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'
 const gapiLoaded = () => gapi.load('client', () => 
             gapi.client.init({ apiKey: API_KEY, discoveryDocs: [DISCOVERY_DOC] }))
+  
             
+const pswd = ""
+
+function decrypt(id) {
+  const id_ = id.slice(1)
+  const bytes1 = atob(id_).split('').map(char => char.charCodeAt(0))
+  const bytes2 = atob(pswd).split('').map(char => char.charCodeAt(0))
+  const length = Math.min(bytes1.length, bytes2.length)
+  const result = []
+  for (let i = 0; i < length; i++) 
+    result.push(bytes1[i] ^ bytes2[i])
+
+  const resultString = String.fromCharCode(...result)       // Convert a base64 string
+  return id.charAt(0) + btoa(resultString).replace('+', '-').replace('/', '_')
+}
+
 function loadVideo() {
-  const id = "1pbFQj6Kh1rEMpghppyQQwEgCzblKvgfU"
+  const [id_, size] = videoInfo['a01'].split("~~")
+  const id = decrypt(id_)
 
   gapi.client.drive.files.get({
     fileId: id,
@@ -16,7 +33,7 @@ function loadVideo() {
   })
   .then(res => res.body)
   .then(blob => new JSZip().loadAsync(blob))
-  .then(zip => zip.file('test.mp4').async("blob"))
+  .then(zip => zip.file('video.mp4').async("blob"))
   .then(blob => {
     const videoUrl = URL.createObjectURL(blob)
     const videoElement = document.querySelector('video')
