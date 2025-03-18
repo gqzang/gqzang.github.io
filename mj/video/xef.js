e0a0fefb8143eefa22b7ec861bc2d0924e1107db
gz <gqzang@gmail.com>
2025-03-18 00:43:54 -0400

    Using multiple step blob constructing method, to solve the memory issue.

--- mj/video/xef.js
+++ mj/video/xef.js
@@ -16,21 +16,29 @@ function xor_crypt(src, mask) {
 }
 
 function xef_decrypt(bStr, mask) {
-  const bytes = strToBytes(bStr)
+  var bytes = strToBytes(bStr.slice(0, 2))
   const linfo = bytes[0] * 256 + bytes[1]
   var cp = 2 + linfo                  // current position in bytes
-  const info = bytesToStr(xor_crypt(bytes.slice(2, cp), mask)).split('|')
+  bytes = strToBytes(bStr.slice(2, cp))
+  const info = bytesToStr(xor_crypt(bytes, mask)).split('|')
   const min_len = parseInt(info[0])
+
   const result = {}
   for(let i = 1; i < info.length; i += 2) {
     const fn = info[i], size = parseInt(info[i+1])
-    const buf = bytes.slice(cp, cp + size)
-    const xbuf = xor_crypt(buf.slice(0, Math.min(min_len, size)), mask)
-    for(let j = min_len; j < size; j ++)
-        xbuf.push(buf[j])
-    const blob = new Blob([new Uint8Array(xbuf)], { type: 'video/mp4' });
-    result[fn] = blob
+    const xStr = bStr.slice(cp, cp + Math.min(min_len, size))
+    const xU8A = new Uint8Array(xor_crypt(strToBytes(xStr), mask))
+    const rU8As = createU8As(bStr, cp + Math.min(min_len, size), cp + size)
+    result[fn] = new Blob([xU8A].concat(rU8As), { type: 'video/mp4' })
     cp += size
   }
   return result  
+}
+
+function createU8As(bStr, a, b) {
+  const U8As = [], MAX = 10*1024*1024                      // 10MB
+  for(let p = a; p < b; p += MAX ) 
+    U8As.push(new Uint8Array(strToBytes(bStr.slice(p, p+MAX))))
+  U8As.push(new Uint8Array(strToBytes(bStr.slice(Math.floor((b-a)/MAX)*MAX, b))))
+  return U8As
 }
