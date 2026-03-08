27f844bcd9a930c2f3bd0abe672a79d2b63fc7a7
gz <gqzang@gmail.com>
2026-03-08 02:11:13 -0400

    CR. Fix the stored video index after loaded.

--- vshow/video.js
+++ vshow/video.js
@@ -1,36 +1,36 @@
 "use strict"
 
-const setProp = (id, disabled, background) => {
-    const ele = de(id); ele.disabled = disabled; ele.style.background = background }
 const getEpoch = () => Math.round((new Date()).getTime() / 1000)
-const cid = de("count"), lid = de("load"), LST = "LoadingStartTime"
-const updateTimer = () => cid.textContent = getEpoch() - parseInt(localStorage.getItem(LST))
-const clearTimer = () => cid.textContent = localStorage.setItem(LST, getEpoch()) | 0
-setInterval(() => { if(!localStorage.getItem(LST)) clearTimer()
-    if(lid.disabled && (de('load').innerText != "Load Video")) updateTimer() }, 1000)
-const selVd = () => de('video').src = vObjs[sId.options[sId.selectedIndex].value]
+const cId = de("count"), lId = de('load'), sId = de('dvd'), vId = de('video'), LST = "LoadStartTime"
+const updateTimer = () => cId.textContent = getEpoch() - parseInt(localStorage.getItem(LST))
+const clearTimer = () => cId.textContent = localStorage.setItem(LST, getEpoch()) | 0
+const selVd = () => vId.src = vObjs[sId.options[sId.selectedIndex].value]
 const listMap = Object.keys(videoBkt).reduce((a, k) => {a[k] = de("list_" + k); return a}, {})
 const vdInfo = x => `${x}: ${(parseInt(videoLen[x])/(1024*1024)).toFixed(2)}`
+const setLoadBtn = x => {const btn = de('load'); 
+                btn.disabled = x; btn.style.background = x ? "lightgray" : "lightgoldenrodyellow"}
 
-let pswd = loadPswd(), id = '', sId = de('dvd'), vObjs = {}
+setInterval(() => { if(!localStorage.getItem(LST)) clearTimer()
+                    if(lId.disabled && (lId.innerText != "Load Video")) updateTimer() }, 1000)
+
+let pswd = loadPswd(), id = '', vObjs = {}
 async function loadV() { if(! videoLen.hasOwnProperty(id)) return alert("No video is selected.")
-    localStorage.setItem(LST, getEpoch()); setProp("load", true, "lightgray")
+    localStorage.setItem(LST, getEpoch()); setLoadBtn(true)
     const mask = b64StrToBytes(pswd), baseUrl = bytesToStr(xor_crypt(b64StrToBytes(baseUrlX), mask))
     const url = baseUrl + videoBkt[id.substring(0,1)] + id + '.xef'  
-    cid.style.color = "green"   // data is loading
+    cId.style.color = "green"   // data is loading
     try { let buf = await (await fetch(url)).arrayBuffer()
-        const videoURL = URL.createObjectURL(xefDecrypt(buf, mask)), newOption = dc('option')
-        de('video').src = vObjs[id] = videoURL; newOption.value = sId.value = id
-        newOption.text = vdInfo(id); sId.add(newOption); de('load').innerText = "Load Video"
-        new Audio("./win.wav").play(); updateTimer(); delete videoLen[id]; loadList()
-    } catch(err) { console.log(err); new Audio("./error.wav").play()
-        setProp("load", false, "lightgoldenrodyellow") }
+        vId.src = vObjs[id] = URL.createObjectURL(xefDecrypt(buf, mask))
+        const newOpt = dc('option'); newOpt.value = id; newOpt.text = vdInfo(id)
+        sId.add(newOpt); sId.value = id; lId.innerText = "Load Video"
+        delete videoLen[id]; loadList(); new Audio("./win.wav").play()
+    } catch(err) { console.log(err); new Audio("./error.wav").play(); setLoadBtn(false) }
 }
+
 const loadList = () => { Object.values(listMap).forEach(le => le.innerHTML = '')  // clear list first
     Object.keys(videoLen).sort().forEach( key => { const newItem = dc('li')
         newItem.textContent = vdInfo(key); newItem.setAttribute('tabindex', 1)
         listMap[key.charAt(0)].appendChild(newItem) })
-    Object.values(listMap).forEach( le => le.onclick = e => {
-        id = e.target.innerHTML.substring(0, 3); de('load').innerText = "Load Video " + id
-        setProp("load", false, "lightgoldenrodyellow"); clearTimer() })
-};    loadList()   // loadList for the first time
+    Object.values(listMap).forEach( le => le.onclick = e => { setLoadBtn(false); clearTimer()
+        id = e.target.innerHTML.substring(0, 3); lId.innerText = "Load Video " + id })
+}; loadList()   // loadList for the first time
