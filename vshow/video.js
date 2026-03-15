66ac06eb0624dcc0ea9be04923ebdeacbf7c5ee3
gz <gqzang@gmail.com>
2026-03-15 06:43:25 -0400

    Use innerText for all
    refer: javascript innertext vs textcontent vs innerhtml

--- vshow/video.js
+++ vshow/video.js
@@ -2,7 +2,7 @@
 
 let sId = de('dvd'), lBtn = de('load'), LV = "Load Video ", loading = false, id = '', vObjs = {}
 const vOp = de('vid'), selVideo = () => vOp.src = vObjs[sId.options[sId.selectedIndex].value]
-const setStat = x => { loading = lBtn.disabled = x; de("cnt").textContent = 0; setLS(LV, getSec())
+const setStat = x => { loading = lBtn.disabled = x; de("cnt").innerText = 0; setLS(LV, getSec())
                        lBtn.style.background = x ? "lightgray" : "lightgoldenrodyellow" }
 
 const loadV = async () => { try { setStat(true); let bb = await getBlob(loadPswd(), vdInfo[id])
@@ -10,9 +10,9 @@ const loadV = async () => { try { setStat(true); let bb = await getBlob(loadPswd
     vOp.src = vObjs[id] = URL.createObjectURL(bb); new Audio("./win.wav").play()
 } catch(err) { console.log(err); new Audio("./error.wav").play() }            loadList() }
 
-const loadList = () => vCat.forEach( x => de(x).innerHTML = '' ) || Object.keys(vdInfo).forEach(
-    k => { const x = dc('li'), c = k.charAt(0); x.textContent = k; x.setAttribute('tabindex', 1)
+const loadList = () => vCat.forEach( x => de(x).innerText = '' ) || Object.keys(vdInfo).forEach(
+    k => { const x = dc('li'), c = k.charAt(0); x.innerText = k; x.setAttribute('tabindex', 1)
            de(c).appendChild(x); loading = false; lBtn.innerText = LV });     loadList() // 1st time
 vCat.forEach( x => de(x).onclick = 
-    e => loading || setStat(false) || ( lBtn.innerText = LV + ( id = e.target.innerHTML ) ) )
-setInterval( () => loading && ( de("cnt").textContent = ( getSec() - getLS(LV) ).toFixed(1) ), 100 )
+    e => loading || setStat(false) || ( lBtn.innerText = LV + ( id = e.target.innerText ) ) )
+setInterval( () => loading && ( de("cnt").innerText = ( getSec() - getLS(LV) ).toFixed(1) ), 100 )
