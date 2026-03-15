3a7799a47e993e09d1fae5af11a4d928c2249156
gz <gqzang@gmail.com>
2026-03-15 02:40:02 -0400

    CR

--- vshow/video.js
+++ vshow/video.js
@@ -1,21 +1,18 @@
 "use strict"
 
 let sId = de('dvd'), lBtn = de('load'), LV = "Load Video ", loading = false, id = '', vObjs = {}
-const clearTimer = () => { de("cnt").textContent = 0; setLS(LV, getSec()) }
-const selVideo = () => clearTimer() || (de('vid').src = vObjs[sId.options[sId.selectedIndex].value])
-const setLoadStat = x => { loading = lBtn.disabled = x; clearTimer()
-                            lBtn.style.background = x ? "lightgray" : "lightgoldenrodyellow" }
+const vOp = de('vid'), selVideo = () => vOp.src = vObjs[sId.options[sId.selectedIndex].value]
+const setStat = x => { loading = lBtn.disabled = x; de("cnt").textContent = 0; setLS(LV, getSec())
+                       lBtn.style.background = x ? "lightgray" : "lightgoldenrodyellow" }
 
-const loadV = async () => { setLoadStat(true); let ref = vdBkt[id.charAt(0)] + id.substring(0, 3) + '.xef'
-    try { de('vid').src = vObjs[id] = URL.createObjectURL( await getBlob(loadPswd(), ref) )
-        const nOpt = dc('option'); delete vdInfo[nOpt.value = nOpt.text = id]
-        sId.add(nOpt); sId.value = id; new Audio("./win.wav").play()
-    } catch(err) { console.log(err); new Audio("./error.wav").play() }                loadList() }
+const loadV = async () => { try { setStat(true); let bb = await getBlob(loadPswd(), vdInfo[id])
+    let nOp = dc('option'); delete vdInfo[nOp.value = nOp.text = id]; sId.add(nOp); sId.value = id
+    vOp.src = vObjs[id] = URL.createObjectURL(bb); new Audio("./win.wav").play()
+} catch(err) { console.log(err); new Audio("./error.wav").play() }            loadList() }
 
 const loadList = () => vCat.forEach( x => de(x).innerHTML = '' ) || Object.keys(vdInfo).forEach(
-    k => {let x = dc('li'); x.textContent = k; x.setAttribute('tabindex', 1)
-        de(k.charAt(0)).appendChild(x); loading = false; lBtn.innerText = LV });     loadList() // 1st time
-
-vCat.forEach( x => de(x).onclick = e => { if(loading) return
-    setLoadStat(false); id = e.target.innerHTML; lBtn.innerText = LV + id } )
-setInterval( () => { if(loading) de("cnt").textContent = ( getSec() - getLS(LV) ).toFixed(1) }, 100 )
+    k => { const x = dc('li'), c = k.charAt(0); x.textContent = k; x.setAttribute('tabindex', 1)
+           de(c).appendChild(x); loading = false; lBtn.innerText = LV });     loadList() // 1st time
+vCat.forEach( x => de(x).onclick = 
+    e => loading || setStat(false) || ( lBtn.innerText = LV + ( id = e.target.innerHTML ) ) )
+setInterval( () => loading && ( de("cnt").textContent = ( getSec() - getLS(LV) ).toFixed(1) ), 100 )
